1b021bcca4673a4cad1883bdb14cf687209b15db
Kheev <goldielpro@uma.es>
2015-03-03 20:33:47 +0100

    commit prueba remota

--- GitAux.java
+++ GitAux.java
@@ -16,11 +16,15 @@ public class GitAux {
     
     public void metodoAlumno2(){
         System.out.println("metodo 2 + metodo 1");
+	System.out.println("modificado por alumno 2");  // alumno 2
+
     }
     
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
-    }
+	// modificaciones alumno 2
+	System.out.println("Alumno 2 inserta esta sentencia");
+	}
     public void testGitAux(){
       // metodo rama testing
       System.out.println ("Metodo de testing");
